9b91c3a5c0e089a89fee6da1d0bc03aee76f06df
Morre Thierry <thierry.morre@cognitic.be>
2021-08-13 17:22:35 +0200

    Ajoutez des fichiers projet.

---
+++ TestApi/Period.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApi
+{
+    class Period
+    {
+        public int Number { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool IsDaytime { get; set; }
+        public int Temperature { get; set; }
+
+        public string WindSpeed { get; set; }
+        public string WindDirection { get; set; }
+        public string ShortForecast { get; set; }
+    }
+
+    /*
+"number": 1,
+                "name": "Today",
+                "startTime": "2021-08-13T11:00:00-04:00",
+                "endTime": "2021-08-13T18:00:00-04:00",
+                "isDaytime": true,
+                "temperature": 98,
+                "temperatureUnit": "F",
+                "temperatureTrend": null,
+                "windSpeed": "3 to 7 mph",
+                "windDirection": "SW",
+                "icon": "https://api.weather.gov/icons/land/day/hot/tsra_hi,40?size=medium",
+                "shortForecast": "Mostly Sunny then Chance Showers And Thunderstorms",
+                "detailedForecast": "A chance of showers and thunderstorms between 2pm and 5pm, then a chance of showers and thunderstorms. Mostly sunny, with a high near 98. Heat index values as high as 107. Southwest wind 3 to 7 mph. Chance of precipitation is 40%. New rainfall amounts between a tenth and quarter of an inch possible."
+     */
+}
---
+++ TestApi/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+using System.Text.Json;
+
+namespace TestApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //"https://api.weather.gov/gridpoints/LWX/96,70/forecast"
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.weather.gov/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("User-Agent", "C# Application");
+
+                HttpResponseMessage httpResponseMessage = client.GetAsync("gridpoints/LWX/96,70/forecast").Result;
+
+                try
+                {
+                    httpResponseMessage.EnsureSuccessStatusCode();
+
+                    string json = httpResponseMessage.Content.ReadAsStringAsync().Result;
+
+                    JsonDocument jsonDocument = JsonDocument.Parse(json);
+                    JsonElement rootElement = jsonDocument.RootElement;
+
+                    JsonElement properties = rootElement.GetProperty("properties");
+                    JsonElement periods = properties.GetProperty("periods");
+
+                    IEnumerable<Period> periodsResult = JsonSerializer.Deserialize<Period[]>(periods.ToString(), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+                    foreach (Period period in periodsResult)
+                    {
+                        Console.WriteLine($"{period.Number} : {period.ShortForecast}");
+                    }
+
+                    Console.WriteLine(json);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                
+
+                
+            }
+
+
+        }
+    }
+}
